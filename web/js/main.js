be9894a2ca6b9372a8104068a51aa8560522d2c7
Daniel Suszek <danielsuszek1@gmail.com>
2015-12-05 18:38:09 +0100

    AJAX works! End of Tutorial 6 - FormType & XmlHttpRequest

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,61 @@
-/*
 $(function(){
-   console.log('Działa main.js');
+   // console.log('Działa main.js');
+   // alert('ok');
+   /*
+   $('#task-complete-1').on({
+      click: function(e){
+          alert('task-complete-1');
+      }
+   });
+   */
+   $('.ajax-task-complete').on({
+      click: function(e){
+          e.preventDefault();
+          var $href = $(this).attr('href');
+          // alert($href);   
+          $('<div></div>').load($href + ' form', function() {
+            // alert($href);
+             
+            // set form
+            var $form = $(this).children('form');
+            
+            // set checkbox
+            var $cb = $form.find('input[type="checkbox"]');
+
+            // toggle
+            $cb.prop('checked', !$cb.prop('checked'));
+
+            // form action url
+            var $url = $form.attr('action');
+            
+            // set data
+            var $data  = $form.serialize();
+            
+            
+            $.ajax({
+                url: $url,
+                data: $data,
+                method: 'post',
+                dataType: 'json',
+                cache: false,
+                success: function(obj) {
+                    var $tic = $('#task-complete-' + obj.id + ' .ajax-task-complete');
+                    if (obj.complete) {
+                        $tic.text('X');
+                    } else {
+                        $tic.text('O');
+                    }
+                },
+                complete: function() {
+                    console.log('complete!');
+                },
+                error: function(err) {
+                    console.log(err);
+                }
+            });
+            
+          });
+          
+      } // end click: function(e){
+   });
 });
-*/
